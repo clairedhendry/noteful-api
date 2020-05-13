3efe9cab1d76c8704ffa28f1fb706f7e3ba5dc4a
Claire Hendry <claire.d.hendry@gmail.com>
2020-05-13 17:10:57 -0400

    full stack deployment

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/noteful',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/noteful-test'
 }
